797487683f88f861d8768f3c7b779a02aa292a07
Tolben <vitek.pasek@gmail.com>
2025-10-22 21:00:36 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/001_table.sql
+++ schema/001_table.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: two_factor
 CREATE TABLE IF NOT EXISTS two_factor (
   user_id BIGINT UNSIGNED NOT NULL,
